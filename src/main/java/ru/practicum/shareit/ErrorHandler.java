e751d23c7c031510ef4b5b13c354f4fae4cc7bf8
bannovdaniil <baddan@mail.ru>
2022-08-14 12:21:31 +0300

    add: Error Handler - обработка ошибок.

---
+++ src/main/java/ru/practicum/shareit/ErrorHandler.java
@@ -0,0 +1,61 @@
+package ru.practicum.shareit;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.item.ItemController;
+import ru.practicum.shareit.user.UserController;
+import ru.practicum.shareit.user.exception.UserEmailExistException;
+import ru.practicum.shareit.user.exception.UserNotFoundException;
+
+import java.security.InvalidParameterException;
+
+@RestControllerAdvice(assignableTypes = {
+        UserController.class,
+        ItemController.class})
+public class ErrorHandler {
+    private ErrorResponse errorResponse;
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            InvalidParameterException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequest(final Exception e) {
+        return new ErrorResponse("Ошибка валидации данных: " + e.getMessage());
+    }
+
+    @ExceptionHandler({
+            UserNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(final Exception e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({
+            UserEmailExistException.class})
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse duplicateIsFound(final Exception e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAllError(final Throwable e) {
+        return new ErrorResponse("Произошла непредвиденная ошибка.");
+    }
+
+    private static class ErrorResponse {
+        private final String error;
+
+        public ErrorResponse(String error) {
+            this.error = error;
+        }
+
+        public String getError() {
+            return error;
+        }
+    }
+}
