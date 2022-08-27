f3e2d1aabff8a422eeaeed4ab192e71eff4b462c
bannovdaniil <baddan@mail.ru>
2022-08-27 15:52:12 +0300

    #3 - ItemRequest model

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,7 +1,10 @@
 package ru.practicum.shareit.request.model;
 
-/**
- * // TODO .
- */
+import java.time.LocalDateTime;
+
 public class ItemRequest {
+    private Long id;
+    private String description;
+    private Long requestor;
+    private LocalDateTime created;
 }
