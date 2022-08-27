9695c2bbce8d513e1e6f5cc6fb77883e276ea6f3
bannovdaniil <baddan@mail.ru>
2022-08-27 15:51:08 +0300

    #2 - Booking model

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,7 +1,12 @@
 package ru.practicum.shareit.booking.model;
 
-/**
- * // TODO .
- */
+import java.time.LocalDateTime;
+
 public class Booking {
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long item;
+    private Long booker;
+    private Status status;
 }
